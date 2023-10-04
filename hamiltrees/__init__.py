dc1d5ddba086cbd15d0b87a4a3ea49d477adcc2b
fiyoooo <fiona.froehler@gmail.com>
2023-10-05 00:38:13 +0200

    Added block_partitioning_MPO.

--- hamiltrees/__init__.py
+++ hamiltrees/__init__.py
@@ -8,6 +8,7 @@ Fermionic Hamiltonian Construction for Tree Tensor Network Architectures.
 
 from hamiltrees import (
     block_partitioning,
+    block_partitioning_MPO,
     expectation_value,
     random_construction
 )
