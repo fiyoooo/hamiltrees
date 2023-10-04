9e3dec06d6262a24d121ec32a158dd352ed7e150
fiyoooo <fiona.froehler@gmail.com>
2023-10-05 00:30:45 +0200

    Moved partitioning to block_partitioning.

--- hamiltrees/random_construction.py
+++ hamiltrees/random_construction.py
@@ -42,35 +42,23 @@ def construct_random_molecular_hamiltonian(L, rng):
     H = qib.operator.MolecularHamiltonian(field, 0., tkin, vint, qib.operator.MolecularHamiltonianSymmetry.HERMITIAN | qib.operator.MolecularHamiltonianSymmetry.VARCHANGE)
     return H
 
-def construct_random_partitioned_hamiltonian(L, LA, rng):
+def construct_random_partitioned_hamiltonian_FO(L, LA, rng):
     """
     Construct a random partitioned molecular Hamiltonian acc. to the paper.
     Returns a FieldOperator.
     """
-    # underlying lattice
-    latt = qib.lattice.FullyConnectedLattice((L,))
-    field = qib.field.Field(qib.field.ParticleType.FERMION, latt)
-
-    # random coefficients
-    tkin, vint = construct_random_coefficients(L, rng)
-
-    # sizes of regions A and B
-    regionA = range(0, LA)
-    regionB = range(LA, L)
-
-    # H_A Hamiltonian
-    HA = ham.block_partitioning.construct_part_of_hamiltonian(regionA, field, tkin, vint)
+    H = construct_random_molecular_hamiltonian(L, rng)
+    return ham.block_partitioning.construct_partitioned_hamiltonian_FO(H, LA)
     
-    # H_B Hamiltonian
-    HB = ham.block_partitioning.construct_part_of_hamiltonian(regionB, field, tkin, vint)
-    
-    # H_{AB} Hamiltonian
-    HAB = ham.block_partitioning.construct_interacting_hamiltonian(regionA, regionB, field, tkin, vint)
+def construct_random_partitioned_hamiltonian_MPO(L, LA, rng):
+    """
+    Construct a random partitioned molecular Hamiltonian acc. to the paper as MPO.
+    Returns an MPO.
+    """
+    H = construct_random_molecular_hamiltonian(L, rng)
+    return ham.block_partitioning_MPO.construct_partitioned_hamiltonian_as_MPO(H, LA)
 
-    return HA + HAB + HB
     
-    
-# copied test_as_vector from fermionic_tensor_networks.code.tests.test_fMPS
 def construct_random_fMPS(L):
     rng = np.random.default_rng()
 
